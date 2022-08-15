5c84aa499fd4c76e2391764aafc6b54705f1e999
ShiroganeS <saito.shirogane@gmail.com>
2022-08-15 12:13:20 +0200

    implement adding games

--- lib/app/home/add_opinion/add_opinion_page_contennt.dart
+++ lib/app/home/add_opinion/add_opinion_page_contennt.dart
@@ -1,14 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var gameName = "";
+  var nameName = "";
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("TRZY"),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: "Podaj nazwę gry",
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                gameName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: "Podaj nazwę Konsoli",
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                nameName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance
+                  .collection("game")
+                  .add({"neme": gameName, "game": nameName, "rating": 3.0});
+            },
+            child: const Text("Dodaj"),
+          ),
+        ],
+      ),
     );
   }
 }
